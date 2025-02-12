1b57f3dde97c15eeb972f4e17d46942580e5242d
Rahul Sharma <r_sharma21@cs.uni-kl.de>
2025-02-12 11:42:58 +0100

    minor adjustments

--- base.py
+++ base.py
@@ -1,11 +1,10 @@
+from typing import Dict, Any, Sequence, Literal, Optional, get_args
 from neo4j import AsyncGraphDatabase
 from dataclasses import dataclass, field
 from collections import namedtuple
-from typing import Dict, Sequence, Any, Literal, Optional, get_args
-from pathlib import Path
 from inflection import underscore
+from uuid import uuid4
 import re
-
 
 from backend.config import config
 
@@ -17,8 +16,8 @@ async def populate(ontology):
     Assumes that the database is already empty. 
     If not, you can clear it by calling `delete_neo4j_database` first.
     """
-    async with AsyncGraphDatabase.driver(config.knowledgeBase.uri, auth=(config.knowledgeBase.username, config.knowledgeBase.password)) as driver:
-        async with driver.session(database=config.knowledgeBase.dbname) as session:
+    async with AsyncGraphDatabase.driver(config.local.uri, auth=(config.local.username, config.local.password)) as driver:
+        async with driver.session(database=config.local.dbname) as session:
             try:
                 tx = await session.begin_transaction()
                 await tx.run("MATCH (n) DETACH DELETE n")
@@ -29,36 +28,70 @@ async def populate(ontology):
                 #             """
                 # )
                 for n in ontology.nodes:
-                    await tx.run(
-                        """
-                        CREATE (n:Node {id: $node_id})
-                        """,
-                        node_id=n.type.title(),
-                    )
+                    await tx.run("CREATE (n:Node {name: $name})", name=n.type)
 
                 for e in ontology.edges:
-                    await tx.run(
-                        f"""
-                        MATCH (n1:Node {{id: $u}}), (n2:Node {{id: $v}})
-                        CREATE (n1)-[r:{underscore(e.type)}]->(n2)
+                    if e.type == 'hasname': continue
+                    await tx.run("""
+                        MATCH (from {name: $s}), (to {name: $o})
+                        CALL apoc.create.relationship(from, $p, {}, to)
+                        YIELD rel
+                        RETURN rel
                         """,
-                        u=e.source.title(),
-                        v=e.destination.title(),
+                        s=e.source,
+                        p=underscore(e.type),
+                        o=e.destination,
                     )
                 await tx.commit()
+                tx = await session.begin_transaction()
+                for e in ontology.edges:
+                    if e.type == 'hasname':
+                        await tx.run("""
+                            MATCH (n {name: $name})
+                            SET
+                              n.uuid = $name, 
+                              n.name = $method
+                            RETURN n;
+                            """,
+                            name=e.source,
+                            method=e.destination,
+                        )
+                await tx.commit()
+                tx = await session.begin_transaction()
+                for e in ontology.edges:
+                    if e.type == 'calls':
+                        await tx.run("""
+                            MATCH (n:Node {uuid: $name})
+                            REMOVE n:Node
+                            SET n:Method
+                            RETURN n;
+                            """,
+                            name=e.destination,
+                        )
+                        await tx.run("""
+                            MATCH (n:Node {name: $name})
+                            REMOVE n:Node
+                            SET n:Method, n.uuid = $uuid
+                            RETURN n;
+                            """,
+                            name=e.destination,
+                            uuid=uuid4().hex
+                        )
+                await tx.commit()                
             except Exception as e:
                 print(f"An error occurred: {e}")
                 return []
-        
-
-EdgeType = Literal['IsA', 'IsSubclassOf', 'Implements', 'IsEquivalentTo', 'HasParameter']
-
-def to_camel(expr: str) -> str:
-    # return re.sub(r'\s(\w)', lambda x: x[1].upper(), expr.title())
-    return expr.replace(' ', '')
 
 
+
+# TODO: Skip triggers fallback
+
+Predicate = Literal['IsSubclassOf', 'Implements', 'IsEquivalentTo', 'HasParameter', 'AppliesTo', 'Has', 'IsAn', 'IsA',
+                    'IsOfType', 'Fallback', 'WithDescription', 'WithParameter', 'Ensures', 'ContributesTo', 'Calls',
+                    'MightIntroduce', 'IsThreatTo', 'IsDimensionOf', 'AttributesTo', 'ShouldEnsure', 'MightMitigate', 'IsSynonymOf', 'IsProtectedAttribute']
 Statement = namedtuple('Statement', 'subject predicate object')
+
+separator = ';'
 camel_case = re.compile(r'(?<!^)(?=[A-Z])')
 knowledge = """
 # Data Science Task is a Core Concept
@@ -70,6 +103,7 @@ Classification is subclass of Supervised Learning
 Regression is subclass of Supervised Learning
 Clustering is subclass of Unsupervised Learning
 Data Collection is subclass of Data Science Task
+Data Loading is subclass of Data Science Task
 Data Cleaning is subclass of Data Preprocessing
 Data Augmentation is subclass of Data Preprocessing
 Data Integration is subclass of Data Preprocessing
@@ -97,28 +131,63 @@ Data Encoding is subclass of Data Preprocessing
 OneHotEncoder is subclass of Data Encoding
 SVM is subclass of Classification
 
+Predictive Performance is a Measure
+Accuracy is a Metric
+Recall is a Metric
+ROC AUC Score is a Metric
+Accuracy contributes to Predictive Performance
+Recall contributes to Predictive Performance
+ROC AUC Score contributes to Predictive Performance
+
+# Evaluation Procedure is a Core Concept
+
+# TODO: 'conditions' vs 'preferences'
+# E.g., for smaller datasets, use cross-validation or LOOCV to make the best use of limited data
+
+# Interface is a Core Concept
+Sklearn Estimator is an Interface
+Sklearn Estimator calls __init__; calls fit; calls predict
+
+Sklearn Transformer is an Interface
+Sklearn Transformer calls __init__; calls fit; calls transform
+
+Function is an Interface
+
 TrainTestSplit might introduce Bias
 
 # TODO: libraries and versions
 sklearn.model_selection.train_test_split is an Operator
-sklearn.model_selection.KFold is an Operator
-sklearn.model_selection.StratifiedKFold is an Operator
-sklearn.preprocessing.MinMaxScaler is an Operator
-sklearn.preprocessing.StandardScaler is an Operator
-sklearn.preprocessing.OneHotEncoder is an Operator
-sklearn.svm.SVC is an Operator
-
 sklearn.model_selection.train_test_split implements TrainTestSplit
+sklearn.model_selection.train_test_split is a Function
+
+sklearn.model_selection.KFold is an Operator
 sklearn.model_selection.KFold implements KFold
+sklearn.model_selection.KFold is a Function
+
+sklearn.model_selection.StratifiedKFold is an Operator
 sklearn.model_selection.StratifiedKFold implements StratifiedKFold
+sklearn.model_selection.StratifiedKFold is a Function
+
+sklearn.preprocessing.MinMaxScaler is an Operator
+sklearn.preprocessing.MinMaxScaler is a Sklearn Transformer
 sklearn.preprocessing.MinMaxScaler implements MinMaxScaler
+
+sklearn.preprocessing.StandardScaler is an Operator
+sklearn.preprocessing.StandardScaler is a Sklearn Transformer
 sklearn.preprocessing.StandardScaler implements StandardScaler
+
+sklearn.preprocessing.OneHotEncoder is an Operator
+sklearn.preprocessing.OneHotEncoder is a Sklearn Transformer
 sklearn.preprocessing.OneHotEncoder implements OneHotEncoder
+
+sklearn.svm.SVC is an Operator
+sklearn.svm.SVC is a Sklearn Estimator
 sklearn.svm.SVC implements SVM
 
 # Requirement is a Core Concept
 # https://digital-strategy.ec.europa.eu/en/library/ethics-guidelines-trustworthy-ai
 # TODO: Add 'has context' with descriptions and definitions
+
 Fairness is a Requirement
 Lawfulness is a Requirement
 Human Agency is a Requirement
@@ -136,14 +205,64 @@ Environmental Well-being is a Requirement
 Accountability is a Requirement
 Acceptance is a Requirement
 
+#Concepts of Fairness
 Equality is subclass of Fairness
 Equity is subclass of Fairness
+Transparency is subclass of Fairness
+Confidentiality is subclass of Fairness
+Voice is subclass of Fairness
+Timeliness is subclass of Fairness
+Impartiality is subclass of Fairness
+Rationality is subclass of Fairness
+Accountability is subclass of Fairness
+Flexibility is subclass of Fairness
+Dignity is subclass of Fairness
 Bias is threat to Fairness
 Equality is dimension of Fairness
 Equity is dimension of Fairness
+
+#Measures of fairness
+Fairness Measure is a Measure
+Demographic Parity is a Fairness Measure
+Statistical Parity is synonym of Demographic Parity
+Equalized odds is a Fairness Measure
+Equal Opportunity is a Fairness Measure
+Predictive Parity is a Fairness Measure
+Error-Rate Parity is a Fairness Measure
+Accuracy Parity is a Fairness Measure
+Individual Fairness is a Fairness Measure
+Distance-based Fairness is synonym of Individual Fairness
+Counterfactual Fairness is a Fairness Measure
+Causal Fairness is a Fairness Measure
+Calibration Fairness is a Fairness Measure
+Ranking Fairness is a Fairness Measure
+Equal mis-opportunity is a Fairness Measure
+Predictive Equality is synonym of Equal mis-opportunity
+Balanced Group is a Fairness Measure
+Average odds is a Fairness Measure
+
+#Metrics of Fairness
+
+
 Bias is a Risk
 Algorithmic Bias is subclass of Bias
 Historical Bias is subclass of Bias
+Data Bias is subclass of Bias
+Representation Bias is subclass of Bias
+Measurement Bias is subclass of Bias
+Omitted variable Bias is subclass of Bias
+Evaluation Bias is subclass of Bias
+Aggregation Bias is subclass of Bias
+User interaction bias is subclass of Bias
+Population Bias is subclass of Bias
+Deployment Bias is subclass of Bias
+Feedback Loop contributes to Bias
+Unconscious Bias is subclass of Bias
+Cognitive Bias is subclass of Bias
+Confirmation Bias is subclass of Bias
+Selection Bias is subclass of Bias
+Reporting Bias is subclass of Bias
+
 
 Person is an Entity
 First Name attributes to Person
@@ -152,6 +271,7 @@ Last Name attributes to Person
 Date of Birth attributes to Person
 Age attributes to Person
 Gender attributes to Person
+Sex attributes to Person
 Race attributes to Person
 Ethnicity attributes to Person
 Disability status attributes to Person
@@ -161,8 +281,15 @@ National origin attributes to Person
 Marital status attributes to Person
 Socioeconomic status attributes to Person
 
-# Gender is protected against Bias
-# Race is protected against Bias
+Gender is a Protected Attribute
+Race is a Protected Attribute
+Sexual orientation is a Protected Attribute
+Race is a Protected Attribute
+Religion is a Protected Attribute
+Ethnicity is a Protected Attribute
+Sex is a Protected Attribute
+Disability status is a Protected Attribute
+National origin is a Protected Attribute
 
 Classification should ensure Fairness
 
@@ -189,13 +316,8 @@ Quantus_Consistency applies to Tabular Data
 
 # X implements Demographic parity
 
-Predictive Performance is a Measure
-Accuracy is a Metric
-Recall is a Metric
-ROC AUC Score is a Metric
-Accuracy contributes to Predictive Performance
-Recall contributes to Predictive Performance
-ROC AUC Score contributes to Predictive Performance
+Immediate Alternatives is a Mitigation Action
+Immediate Alternatives might mitigate Bias
 """
 
 
@@ -203,14 +325,22 @@ class UnknownRelation(Exception):
     pass
 
 
-def parse(statement: str) -> Statement:
-    # edges = [camel_case.sub(r' ', edge).lower() for edge in get_args(EdgeType)]
-    edges = list(map(underscore, get_args(EdgeType)))
+def parse(statement: str) -> Sequence[Statement]:
+    assert 'has name' not in get_args(Predicate)
+    edges = list(map(underscore, get_args(Predicate)))
+    part, *rest = statement.split(separator)
     for edge in edges:
         e = edge.replace('_', ' ')
-        if e in statement:
-            s, o = statement.split(e)
-            return Statement(subject=s.strip(), predicate=edge, object=o.strip())
+        if e in part:
+            s, o = part.split(e)
+            if rest:
+                _id = uuid4().hex
+                return [Statement(subject=s.strip(), predicate=edge, object=_id),
+                        Statement(subject=_id, predicate='has name', object=o.strip())] \
+                        + parse(_id + separator.join(rest))
+            else:
+                return [Statement(subject=s.strip(), predicate=edge, object=o.strip())]
+            
     raise UnknownRelation(statement)
 
 
@@ -226,7 +356,7 @@ class Node:
 class Edge:
     source: str
     destination: str
-    type: EdgeType
+    type: Predicate
     attr: Optional[Dict[str, Any]] = field(default_factory=dict)
 
 
@@ -236,8 +366,13 @@ class Ontology:
     edges: Sequence[Edge]
 
 
+def to_camel(expr: str) -> str:
+    # return re.sub(r'\s(\w)', lambda x: x[1].upper(), expr.title())
+    return expr.replace(' ', '')
+
+
 def get_ontology() -> Ontology:
-    stmts = [parse(line) for line in knowledge.strip().split('\n') if line and not line.startswith('#')]
+    stmts = [el for line in knowledge.strip().split('\n') if line and not line.startswith('#') for el in parse(line)]
     return Ontology(
         nodes=list(set([Node(type=el) for st in stmts for el in [st.subject, st.object]])),
         edges=list(map(lambda el: Edge(source=el.subject,
